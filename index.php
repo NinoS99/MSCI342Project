8e1e1a2ecc2d9972cd2d04b87c2295699e651e9d
Jason Mah <72579412+j7mah@users.noreply.github.com>
2020-11-07 19:22:45 -0500

    Updated index file

---
+++ index.php
@@ -0,0 +1,93 @@
+<!--
+
+Group 6 Sprint #1 Credit Card Application Web Page
+Sunday October 25th 2020
+Landing/Homepage page for MSCI442 Credit Card Application Project Sprint #1
+
+-->
+<?php
+?>
+<html>
+	<!--Start head-->
+	<head>
+		<!--Create text styles-->
+        <style type = "text/css"> h1 {color:black}</style>
+		
+		<!--Import relevant style sheets-->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		
+	<!--End head-->
+	</head>
+	
+	<body>
+        
+           <!-- First section header-->
+         <div class="titlebox">
+            <center>
+            </br><h1>Welcome to CreditSimple!</h1></br>
+        </div>
+       
+    <!--Formatting the look of the titlebox-->
+    <style>
+        div.titlebox {
+        background-color: #8FC9FF;
+            width: 658px;
+            height: 119px;
+            left: 402px;
+            top: 64.5px; 
+            font-size: 16px;
+            margin-left: auto;
+            margin-right: auto;
+             width: 50%;
+        }
+        </style>
+        
+        <!-- Spacing between titlebox and button-->
+    <span style="display:block; height: 150;"></span>
+   
+    <!--Creating the button to the login page-->
+    <div class="wrapper">
+      <a href="login.html"><button class="buttonSurvey"></br><h1>Click here to find your ideal credit card!</h1></br></button></a>
+    </div>
+
+    <!--Creating the button to the KnowledgeBase-->
+    <div class="wrapper">
+      <a href="knowledgeBase.html"><button class="buttonKnowledgeBase"></br><h1>CreditSimple KnowledgeBase</h1></br></button></a>
+    </div>
+
+<!--Formatting the look of the buttons-->
+<style>
+   .wrapper {
+    text-align: center;
+}
+    .buttonSurvey {
+        background-color: #DCF790;
+        border: none;
+        text-align: center;
+        width: 469px;
+        height: 142px;
+        left: 0px;
+        top: 370px;    
+        font-size: 10px;
+        float: left;
+        margin-left: 15px;
+}
+
+    .buttonKnowledgeBase {
+        background-color: #DCF790;
+        border: none;
+        text-align: center;
+        width: 469px;
+        height: 142px;
+        left: 0px;
+        top: 370px;    
+        font-size: 10px;
+        float: right;
+        margin-right: 15px;
+    }
+</style>
+
+	</body>
+	
+</html>
