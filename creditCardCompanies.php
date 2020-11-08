e15f7b83876ee3ed0b1028f9794fab92edd0e539
j7mah <j7mah@uwaterloo.ca>
2020-11-07 19:14:12 -0500

    updated knowledge base pages

---
+++ creditCardCompanies.php
@@ -0,0 +1,57 @@
+<?php
+?>
+<html>
+    <head>
+    <!--Create text styles-->
+		<link rel = 'stylesheet' type = 'text/css' href = 'style.css'>
+		<!--Import relevant style sheets-->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">	
+	<!--End head-->
+    </head>
+
+    <body>
+        <!--Title block-->
+        <div class="title">
+            <center>
+            </br><h1>CreditSimple Knowledge Base</h1></br>
+        </div>
+    
+<!--List major credit card companies with corresponding photos-->
+    <center>   
+    <h2> Below are the top three major credit card companies in the world!</h2> <br>
+    <br> <img src="visa.jpg"> 
+    <img src="mastercard.jpg" style="width:394px;height:222px;">
+    <img src="amex.jpeg" style="width:394px;height:222px;">
+      
+<!--Button to allow user to go back to the knowledge base page-->
+    <div>
+      <a href="knowledgeBase.php"><button class="buttonReturn"></br><h1>Return to CreditSimple Knowledge Base</h1></br></button></a>
+    </div>
+
+<!--Formatting the look of the title block and return button-->
+<style>
+   div.title {
+         background-color: #8FC9FF;
+         width: 658px;
+         height: 119px;
+         left: 402px;
+         top: 64.5px; 
+         margin-left: auto;
+         margin-right: auto;
+         width: 50%;
+        }
+    
+    .buttonReturn {
+        background-color: #EA7E77;
+        border: none;
+        text-align: center;
+        width: 400px;
+        height: 150px;
+        margin:30px auto;
+    }
+</style>
+    
+    <body>
+
+</html>
---
+++ typesOfCreditCards.php
@@ -0,0 +1,113 @@
+<?php
+?>
+
+<html>
+    <head>
+    <!--Create text styles-->
+		<link rel = 'stylesheet' type = 'text/css' href = 'style.css'>
+		<!--Import relevant style sheets-->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">	
+	<!--End head-->
+    </head>
+
+    <body>
+        <!--Title block-->
+        <div class="title">
+            <center>
+            </br><h1>CreditSimple Knowledge Base</h1>
+        </div>
+    
+<!--Outline of low interest credit cards-->
+    <h3> Low Interest Credit Cards </h3>
+    <h5> Practical reasons to use a Low Interest credit card </h5> 
+<!--List of practical reasons-->
+    <ul>
+    <h5> <li> Make purchases: A low interest credit card can help you pay less interest than you would on a regular credit card, if you can’t pay off the balance each month. </li> </h5> 
+    <h5> <li> Make balance transfers: Whether you carry a balance on one or multiple cards, you can transfer all your balances onto one low interest credit card; this means you’ll have only one monthly payment at a much lower interest rate. Alternatively, if you have a mortgage, you could consider consolidating your debt and refinancing to one lower mortgage rate. </li> </h5>
+<!--List of pros and cons-->
+    </ul>
+    <h5> Pros of Low Interest Credit Cards: <h5>
+    <ul>
+    <h5> <li> Low interest rate on purchases, cash advances and balance transfers </li> </h5>
+    <h5> <li> Low annual fees </li> </h5>
+   </ul>
+    <h5> Cons of Low Interest Credit Cards: <h5>
+    <ul>
+    <h5> <li> Fewer rewards or benefits, such as insurance coverage or travel benefits </li> </h5>
+    <h5> <li> Low interest rates may cause some people to rack up consumer debt </h5> </li>
+   </ul>
+
+<!--Outline of reward credit cards-->
+    <h3> Rewards Credit Cards:</h3>
+<!--List of practical reasons-->
+    <h5> Practical reasons to use a Rewards credit card: </h5> 
+    <ul>
+    <h5> <li> "Free rewards": In return for signing up and using the card, you accumulate points or credits to eventually redeem for something of significant value, such as a flight or vacation </li> </h5> 
+    <h5> <li> Types of reward programs: Travel credit (offer travel points every time you make a purchase, which can be redeemed for things like plane tickets or hotel rooms), store credit (linked to specific retailers and offer points that can be redeemed for merchandise) etc. </li> </h5> 
+<!--List of pros and cons-->
+    </ul>
+    <h5> Pros of Rewards Credit Cards: <h5>
+    <ul>
+    <h5> <li> If you’re already making purchases using your credit card, you might as well accumulate rewards along the way </li> </h5>
+    <h5> <li> Rewards can help you save money on future purchases </li> </h5>
+   </ul>
+    <h5> Cons of Rewards Credit Cards: <h5>
+    <ul>
+    <h5> <li> To accumulate and redeem your rewards, you must be in “good standing”, meaning you are not delinquent in making your minimum monthly payment </li> </h5>
+    <h5> <li> Rewards cards tend to carry higher interest rates and are more likely to have annual fees than cards without perks </li> </h5>
+    <h5> <li> Some rewards expire after a certain period of time </li> </h5>
+    </ul>
+     
+<!--Outline of student credit cards-->
+    <h3> Student Credit Cards: </h3>
+<!--List of practical reasons-->
+    <h5> Practical reasons to use a Rewards credit card: </h5> 
+    <ul>
+    <h5> <li> Prepare for the future: When you’re in college or university, one of the most important things you can do for your future is building your credit. In the real world, you need a good credit history to apply for an apartment to rent, finance a car and, eventually, apply for a mortgage to buy a home </li> </h5> 
+<!--List of pros and cons-->
+    </ul>
+    <h5> Pros of Student Credit Cards: <h5>
+    <ul>
+    <h5> <li> There are no (or low) annual fees </li> </h5>
+    <h5> <li> It will help you establish your credit history </li> </h5>
+    <h5> <li> You can learn how to manage your money responsibly </li> </h5>
+   </ul>
+    <h5> Cons of Student Credit Cards: <h5>
+    <ul>
+    <h5> <li> They come with fewer rewards or benefits</li> </h5>
+    <h5> <li> They have high interest rates on outstanding balances and cash advances </li> </h5>
+    <h5> <li> If not used responsibly, a credit card could cause some students to rack up debt they can’t afford to repay </li> </h5>
+    </ul>
+
+<!--Button to allow user to go back to the knowledge base page-->
+    <div>
+      <a href="knowledgeBase.php"><button class="buttonReturn"></br><h1>Return to CreditSimple Knowledge Base</h1></br></button></a>
+    </div>
+
+<!--Formatting the look of the title block and return button-->
+<style>
+   div.title {
+         background-color: #8FC9FF;
+         width: 658px;
+         height: 119px;
+         left: 402px;
+         top: 64.5px; 
+         margin-left: auto;
+         margin-right: auto;
+         width: 50%;
+        }
+    
+    .buttonReturn {
+        background-color: #EA7E77;
+        border: none;
+        text-align: center;
+        width: 400px;
+        height: 150px;
+        margin-left: 600px;
+    }
+</style>
+    
+    <body>
+
+</html>
