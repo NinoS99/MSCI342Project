35c132c6a9d6959bbb4f62242e26d8c456c20a33
dylanh107 <dylanh107@gmail.com>
2020-11-08 18:03:56 -0800

    Changed titleboxes in KnowledgeBase to jumbotron titles (bootstrap)
    modified title boxes in the KnowledgeBase

--- creditCardApplications.php
+++ creditCardApplications.php
@@ -2,7 +2,15 @@
 ?>
 <html>
     <head>
-    <!--Create text styles-->
+        <!--Bootstrap CDN-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+        
+        <!--Link CSS file-->
+        <link rel="stylesheet" href="style.css">    
+    
+        <!--Create text styles-->
 		<link rel = 'stylesheet' type = 'text/css' href = 'style.css'>
 		<!--Import relevant style sheets-->
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,10 +19,13 @@
     </head>
 
     <body>
-        <!--Title block-->
-        <div class="title">
-            <center>
-            </br><h1>CreditSimple Knowledge Base: Application Basics</h1></br>
+        
+        <!-- Jumbotron titlebox-->
+        <div class = "jumbotron">
+            <h1 class="text-primary">
+            <p class="display-3">CreditSimple KnowledgeBase</p>
+            <small class="text-muted">Application Basics</small>
+            </h1>
         </div>
     
 <!--Outline of credit card application requirements-->
