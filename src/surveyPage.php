53ee1468d80fbcae5a430aea0fb79991099dad08
OmnipotentJon <jc2black@uwaterloo.ca>
2020-11-22 18:48:12 -0500

    Option fields now pull from the database, email is now forced to be and emailand maximum fees is forced to be a number.

--- src/surveyPage.php
+++ src/surveyPage.php
@@ -7,6 +7,7 @@
   //  Make option based fields pull available options from database
   //  HTML Formatting and Seperation of fields
   //  May be more pls look and see - Jonathan
+  // JONATHAN WORKING ON POPULATION OF OPTIONS FROM DTB AND FIELD RESTRICTIONS
   //
   session_start();
   include_once('../my_connect.php');
@@ -73,32 +74,42 @@
     <link rel = 'stylesheet' type = 'text/css' href = 'style.css'>
     <h1>- User Survey - </h1>
       <form method="post" enctype="multipart/form-data">
-        <label for = 'email' > Email </label> <!-- Intake Customer Email (maybe we should add more data/different customer details to intake -->
-        <input type = 'text' id = 'email' name = 'email' value="<?php
+        <label for = 'email' > Email </label> <!-- Intake Customer Email  -->
+        <input type = 'email' id = 'email' name = 'email' value="<?php
         echo isset($_POST['email']) ? $_POST['email'] : '';
-        ?>">
+        ?>" autofocus>
         <br>
         <br>
-        <label for = 'firstName' > First Name </label> <!-- Intake Customer First Name (maybe we should add more data/different customer details to intake -->
-        <input type = 'text' id = 'firstName' name = 'firstName' value="<?php
+        <label for = 'firstName' > First Name </label> <!-- Intake Customer First Name  -->
+        <input type = 'text' id = 'firstName' name = 'firstName' maxlength="30" value="<?php
         echo isset($_POST['firstName']) ? $_POST['firstName'] : '';
         ?>">
         <br>
           <br>
         <label for = 'lastName' > Last Name </label> <!-- Intake Customer Last Name -->
-        <input type = 'text' id = 'lastName' name = 'lastName' value="<?php
+        <input type = 'text' id = 'lastName' name = 'lastName' maxlength="30" value="<?php
         echo isset($_POST['lastName']) ? $_POST['lastName'] : '';
         ?>">
         <br>
           <br>
             <br>
 
+
+
+
       <label for='incomeRange' > Income Range </label> <!-- Justify better and add positioning -->
-      <select name='incomeRange' id = 'incomeRange'> <!-- Intake income range -->
-        <option value='$0 - $30,000'>$0 to $30,000</option>
-        <option value='$30,000 - $100,000'>$30,000 to $100,000</option>
-        <option value='$100,000+'>$100,000 and Up</option>
-      </select>
+      <?php
+            $mysqli = get_mysqli_conn();
+            $resultSet = $mysqli -> query("SELECT DISTINCT incomeRange FROM credit_cards");
+            $mysqli -> query($resultSet); //output selection criteria
+
+            echo '<select name="incomeRange" id = "incomeRange">';
+            while ($row = $resultSet->fetch_assoc()) {
+                  echo '<option value="'.$row['incomeRange'].'">'.$row['incomeRange'].'</option>';
+            }
+
+            echo '</select>';
+      ?>
         <br>
           <br>
       <label for='student_bool' > Are you a student registered in a University or College? </label> <!-- -->
@@ -109,53 +120,96 @@
         <br>
           <br>
       <label for='rewardType' > Reward Type </label>  <!-- Intake reward type details -->
-      <select name='rewardType' id = 'rewardType'>
-        <option value='Points'>Points</option>
-        <option value='Cash Back'>Cash Back</option>
-        <option value='Travel'>Travel</option> <!--  Mabybe list should be generated from the database -->
-      </select>
+      <?php
+            $mysqli = get_mysqli_conn();
+            $resultSet = $mysqli -> query("SELECT DISTINCT reward_type FROM credit_cards");
+            $mysqli -> query($resultSet); //output selection criteria
+
+            echo '<select name="rewardType" id = "rewardType">';
+            while ($row = $resultSet->fetch_assoc()) {
+                  echo '<option value="'.$row['reward_type'].'">'.$row['reward_type'].'</option>';
+            }
+
+            echo '</select>';
+      ?>
         <br>
           <br>
       <label for='averageMonthlySpending' > What is your average monthly spending? </label>  <!-- Intake reward type details -->
-      <select name='averageMonthlySpending' id = 'averageMonthlySpending'>
-        <option value='$0 - $500'>$0 - $500</option>
-        <option value='$500 - $1000'>$500 - $1000</option>
-        <option value='$1000 - $2000'>$1000 - $2000</option>
-        <option value='$2000 - $5000'>$2000 - $5000</option>
-        <option value='$5000+'>$5000 and Up</option><!--  Possibly make this a slider? -->
-      </select>
+      <?php
+            $mysqli = get_mysqli_conn();
+            $resultSet = $mysqli -> query("SELECT DISTINCT averageMonthlySpendingRange FROM credit_cards");
+            $mysqli -> query($resultSet); //output selection criteria
+
+            echo '<select name="averageMonthlySpending" id = "averageMonthlySpending">';
+            while ($row = $resultSet->fetch_assoc()) {
+                  echo '<option value="'.$row['averageMonthlySpendingRange'].'">'.$row['averageMonthlySpendingRange'].'</option>';
+            }
+
+            echo '</select>';
+      ?>
         <br>
           <br>
       <label for='creditScoreKnown' > Do you know your credit score? </label>
       <input type='checkbox' id='creditscoreKnown' name='creditScoreKnown' value='true' onclick='changeStatus(this)'/> <!-- Can we make the value of this show the selection form below if the checkbox is true -->
       <label for='What range is your credit score in?' > What range is your credit score in? </label>  <!-- Intake reward type details -->
-      <select name='creditScore' id='creditScore'>//disabled='disabled'
-        <option value='0 - 200'>0 - 200</option>
-        <option value='200 - 400'>200 - 400</option>
-        <option value='400 - 600'>400 - 600</option>
-        <option value='600 - 800'>600 - 800</option>
-        <option value='800 - 900'>800 - 900</option>
-      </select>
+      <?php
+            $mysqli = get_mysqli_conn();
+            $resultSet = $mysqli -> query("SELECT DISTINCT creditScoreRange FROM credit_cards");
+            $mysqli -> query($resultSet); //output selection criteria
+
+            echo '<select name="creditScore" id = "creditScore">';
+            while ($row = $resultSet->fetch_assoc()) {
+                  echo '<option value="'.$row['creditScoreRange'].'">'.$row['creditScoreRange'].'</option>';
+            }
+
+            echo '</select>';
+      ?>
           <br>
             <br>
        <label for = 'annualFee' > What is the maximum annual fee you are willing to incur? </label>
-          <input type = 'text' id = 'annualFee' name = 'annualFee' value="<?php
+          <input type = 'number' id = 'annualFee' name = 'annualFee' value="<?php
           echo isset($_POST['annualFee']) ? $_POST['annualFee'] : '';
           ?>">
           <br>
             <br>
-        <label for='prefferedInstitution' > What is your preffered institution/bank? </label>  <!-- Preffered Instituation (or cards you already have? Which would kinda tell you) - also options should pull from database -->
-        <select name='prefferedInstitution' id = 'prefferedInstitution'>
-          <option value='none'>none</option>
-          <option value='TD Canada'>TD Canada</option>
-          <option value='ScotiaBank'>ScotiaBank</option>
-          <option value='BMO'>BMO</option>
-          <option value='CIBC'>CIBC</option>
-          <option value='Libro'>Libro</option>
-        </select>
+
+        <label for='prefferedInstitution' > What is your preffered institution/bank? </label>  <!-- Preffered Instituation  -->
+
+
+      <?php
+            $mysqli = get_mysqli_conn();
+            $resultSet = $mysqli -> query("SELECT DISTINCT credit_card_company FROM credit_cards");
+            $mysqli -> query($resultSet); //output selection criteria
+
+            echo '<select name="prefferedInstitution" id = "prefferedInstitution">';
+            while ($row = $resultSet->fetch_assoc()) {
+                  echo '<option value="'.$row['credit_card_company'].'">'.$row['credit_card_company'].'</option>';
+            }
+            echo '<option value="none">none</option>';
+            echo '</select>';
+      ?>
+
+
+
+
           <br>
             <br>
       <button type = 'submit' name = 'create'>Submit Survey</button> <!--  Formatting of Submit survey button can improve -->
+      <style>
+              .container {
+                height: 200px;
+                position: relative;
+                border: 3px solid green;
+              }
+
+              .vertical-center {
+                margin: 0;
+                position: absolute;
+                top: 50%;
+                -ms-transform: translateY(-50%);
+                transform: translateY(-50%);
+              }
+        </style>
       </form>
 </html>
 
